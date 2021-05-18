6da9e477929a5dc765897af0c017778c2bdc58a4
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-05-18 21:16:56 +0200

    Bugfix Bump version

--- src/dataRequestToHttp/getMany/index.js
+++ src/dataRequestToHttp/getMany/index.js
@@ -2,7 +2,7 @@ import { stringify } from "query-string"
 
 const getMany = (params, apiUrl, resource) => {
   const query = {
-    filter: JSON.stringify({ id: params.ids })
+    query: JSON.stringify({ id: params.ids })
   }
   return `${apiUrl}/${resource}?${stringify(query)}`
 }
